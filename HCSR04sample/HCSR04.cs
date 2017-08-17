8e60a1aab33693dcf512aeb05796debc858814eb
psychoria <psychoria@hotmail.com>
2017-08-17 23:58:09 +0900

    Initial commit

---
+++ HCSR04sample/HCSR04.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace PGBox.IoTCore.Sensor.Distance
+{
+    class HCSR04
+    {
+        private GpioController gpio;
+        private GpioPin trigPin;
+        private GpioPin echoPin;
+
+        private bool initialized = false;
+        public int TimeoutMilliseconds { get; set; }
+
+        public HCSR04()
+        {
+        }
+
+        public int Init(int trigPinNum, int echoPinNum, int timeoutMilliseconds = 20)
+        {
+            gpio = GpioController.GetDefault();
+            if (null == gpio)
+            {
+                return 1;
+            }
+
+            trigPin = gpio.OpenPin(trigPinNum);
+            echoPin = gpio.OpenPin(echoPinNum);
+
+            TimeoutMilliseconds = timeoutMilliseconds;
+
+            trigPin.SetDriveMode(GpioPinDriveMode.Output);
+            echoPin.SetDriveMode(GpioPinDriveMode.Input);
+
+            initialized = true;
+
+            return 0;
+        }
+    
+        public double GetDistance(int timeoutMilliseconds = 20)
+        {
+            if (false == initialized)
+                return 0.0;
+
+            TimeoutMilliseconds = timeoutMilliseconds;
+
+            trigPin.Write(GpioPinValue.Low);
+            Task.Delay(TimeSpan.FromMilliseconds(1)).Wait(); // 1millisecond
+
+            trigPin.Write(GpioPinValue.High);
+            Task.Delay(TimeSpan.FromMilliseconds(0.01)).Wait(); // 10microseconds
+            trigPin.Write(GpioPinValue.Low);
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            while (sw.ElapsedMilliseconds < TimeoutMilliseconds && GpioPinValue.Low == echoPin.Read()) ;
+
+            if (sw.ElapsedMilliseconds >= TimeoutMilliseconds)
+                return 0.0;
+
+            sw.Restart();
+
+            while (sw.ElapsedMilliseconds < TimeoutMilliseconds && GpioPinValue.High == echoPin.Read()) ;
+            sw.Stop();
+
+            if (sw.ElapsedMilliseconds >= TimeoutMilliseconds)
+                return 0.0;
+
+            // the utralsonic speed is 34300cm/s -> 34.3cm/ms. And divide by 2 becuase it is round-trip time.
+            return sw.Elapsed.TotalMilliseconds * 34.3 / 2.0;
+        }
+    }
+}
---
+++ HCSR04sample/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using PGBox.IoTCore.Sensor.Distance;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HCSR04sample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private const int TRIG_GPIO = 18;
+        private const int ECHO_GPIO = 12;
+
+        private DispatcherTimer timer;
+
+        HCSR04 distanceSensor = new HCSR04();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            int result = distanceSensor.Init(18, 12);
+            if (result != 0)
+                return;
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        public void Timer_Tick(object sender, object e)
+        {
+            distanceText.Text = String.Format("Distance : {0:f} cm", distanceSensor.GetDistance());
+        }
+    }
+}
